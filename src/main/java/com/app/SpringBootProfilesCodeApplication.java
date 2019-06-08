810d78e1eb37ea16b7e9759cd6deba55c7ed1684
Rachit <Rachit@Rachit-PC>
2019-06-09 00:17:44 +0530

    Done

---
+++ src/main/java/com/app/SpringBootProfilesCodeApplication.java
@@ -0,0 +1,14 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProfilesCodeApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hi");
+		SpringApplication.run(SpringBootProfilesCodeApplication.class, args);
+	}
+
+}
