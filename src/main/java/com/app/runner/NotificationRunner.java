376babe3dde64613214a02d05c00bb2c8a1b8e8a
Rachit <Rachit@Rachit-PC>
2019-06-09 22:14:50 +0530

    Code Done

--- src/main/java/com/app/runner/NotificationRunner.java
+++ src/main/java/com/app/runner/NotificationRunner.java
@@ -1,14 +1,20 @@
 package com.app.runner;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import com.app.service.INotificationService;
 
 @Component
 public class NotificationRunner implements CommandLineRunner{
 
-	@Override
+	@Autowired
+	private INotificationService service;
+	
 	public void run(String... args) throws Exception {
 		System.out.println("In Runner");
+		service.notificate();
 	}
 
 }
